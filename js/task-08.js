a5e1dd9695129e1f12d215c013ea4c66bd82d654
Oksana Fylypchak <107584846+Kseniya1417@users.noreply.github.com>
2022-11-13 17:32:14 +0100

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const login = document.querySelector(".login-form");
+const validateForm = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
 
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  const loginData = { email: email.value, password: password.value };
+  console.log(`Email: ${loginData.email}, Password: ${loginData.password}`);
+  event.currentTarget.reset();
+};
+login.addEventListener("submit", validateForm);
